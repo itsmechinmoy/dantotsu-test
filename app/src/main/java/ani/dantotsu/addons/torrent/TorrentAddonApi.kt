d9054ec18b905b10fab489de98eb26ffff3076bb
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-05-07 16:48:59 +0530

    Update TorrentAddonApi.kt

--- app/src/main/java/ani/dantotsu/addons/torrent/TorrentAddonApi.kt
+++ app/src/main/java/ani/dantotsu/addons/torrent/TorrentAddonApi.kt
@@ -2,7 +2,32 @@ package ani.dantotsu.addons.torrent
 
 import eu.kanade.tachiyomi.data.torrentServer.model.Torrent
 
+data class TorrentStreamingSettings(
+    val enableDLNA: Boolean = false,
+    val friendlyName: String = "Dantotsu",
+    val disableUTP: Boolean = false,
+    val disableDHT: Boolean = false,
+    val disablePEX: Boolean = false,
+    val disableLSD: Boolean = false,
+    val enableIPv6: Boolean = false,
+    val disableUPNP: Boolean = false,
+    val nzbDomain: String = "",
+    val nzbPort: Int = 0,
+    val nzbLogin: String = "",
+    val nzbPassword: String = "",
+    val nzbPoolSize: Int = 4,
+)
+
 interface TorrentAddonApi {
+
+    fun applyStreamingSettings(settings: TorrentStreamingSettings) {
+        // Default implementation for backward compatibility with add-ons that don't override this method.
+    }
+
+    fun startServer(path: String, settings: TorrentStreamingSettings) {
+        applyStreamingSettings(settings)
+        startServer(path)
+    }
 
     fun startServer(path: String)
 
@@ -19,6 +44,23 @@ interface TorrentAddonApi {
         data: String,
         save: Boolean,
     ): Torrent
+
+    fun addTorrent(
+        link: String,
+        title: String,
+        save: Boolean,
+    ): Torrent {
+        return addTorrent(link, title, "", "", save)
+    }
+
+    fun uploadTorrent(
+        file: ByteArray,
+        fileName: String,
+        title: String,
+        save: Boolean,
+    ): Torrent {
+        throw UnsupportedOperationException("Torrent upload is not supported by this add-on version")
+    }
 
     fun getLink(torrent: Torrent, index: Int): String
 }
