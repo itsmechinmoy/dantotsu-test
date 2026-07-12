229cfeeab6c00f60997d9b4ef38a7f7dfa7cc031
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-07-13 00:34:15 +0530

    Update MangaReadAdapter.kt

--- app/src/main/java/ani/dantotsu/media/manga/MangaReadAdapter.kt
+++ app/src/main/java/ani/dantotsu/media/manga/MangaReadAdapter.kt
@@ -258,9 +258,16 @@ class MangaReadAdapter(
                         val url = sourceHttp?.baseUrl
                         url?.let {
                             refresh = true
+                            val headersMap = try {
+                                sourceHttp.headers.toMultimap()
+                                    .mapValues { it.value.getOrNull(0) ?: "" }
+                            } catch (e: Exception) {
+                                emptyMap()
+                            }
                             val intent =
                                 Intent(fragment.requireContext(), CookieCatcher::class.java)
                                     .putExtra("url", url)
+                                    .putExtra("headers", headersMap as HashMap<String, String>)
                             startActivity(fragment.requireContext(), intent, null)
                         }
                     }
