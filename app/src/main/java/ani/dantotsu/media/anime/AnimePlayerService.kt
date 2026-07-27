3bec0a9bb25e84e2ea9b7f560858ad27e7d6c468
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-07-27 23:00:45 +0530

    Create AnimePlayerService.kt

---
+++ app/src/main/java/ani/dantotsu/media/anime/AnimePlayerService.kt
@@ -0,0 +1,51 @@
+package ani.dantotsu.media.anime
+
+import android.content.Context
+import android.content.Intent
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.session.DefaultMediaNotificationProvider
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+import ani.dantotsu.R
+
+@OptIn(UnstableApi::class)
+class AnimePlayerService : MediaSessionService() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val notificationProvider = DefaultMediaNotificationProvider.Builder(this)
+            .setChannelName(R.string.video)
+            .setSmallIcon(R.drawable.mono)
+            .build()
+        setMediaNotificationProvider(notificationProvider)
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+        return activeSession
+    }
+
+    companion object {
+        var activeSession: MediaSession? = null
+
+        fun start(context: Context, session: MediaSession) {
+            activeSession = session
+            val intent = Intent(context, AnimePlayerService::class.java)
+            try {
+                context.startService(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        fun stop(context: Context) {
+            activeSession = null
+            val intent = Intent(context, AnimePlayerService::class.java)
+            try {
+                context.stopService(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
