b5917f8a23e2b1998e29f67d4f83720e6ee6a033
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-07-28 09:47:35 +0530

    Update AnimePlayerService.kt

--- app/src/main/java/ani/dantotsu/media/anime/AnimePlayerService.kt
+++ app/src/main/java/ani/dantotsu/media/anime/AnimePlayerService.kt
@@ -1,8 +1,12 @@
 package ani.dantotsu.media.anime
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.annotation.OptIn
+import androidx.core.content.ContextCompat
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.DefaultMediaNotificationProvider
 import androidx.media3.session.MediaSession
@@ -14,10 +18,31 @@ class AnimePlayerService : MediaSessionService() {
 
     override fun onCreate() {
         super.onCreate()
+        val channelId = "anime_player_channel"
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                getString(R.string.video),
+                NotificationManager.IMPORTANCE_LOW
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
         val notificationProvider = DefaultMediaNotificationProvider.Builder(this)
+            .setChannelId(channelId)
             .setChannelName(R.string.video)
             .build()
         setMediaNotificationProvider(notificationProvider)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        activeSession?.let { session ->
+            if (!sessions.contains(session)) {
+                addSession(session)
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
@@ -31,7 +56,7 @@ class AnimePlayerService : MediaSessionService() {
             activeSession = session
             val intent = Intent(context, AnimePlayerService::class.java)
             try {
-                context.startService(intent)
+                ContextCompat.startForegroundService(context, intent)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
