da0ac36fda34afe950dcfc7afcc1db9749699267
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-01 12:15:37 +0530

    Update ConfigurableSource.kt

--- app/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/ConfigurableSource.kt
@@ -22,9 +22,7 @@ interface ConfigurableSource : MangaSource {
 
 fun ConfigurableSource.preferenceKey(): String = "source_$id"
 
-// TODO: use getSourcePreferences once all extensions are on ext-lib 1.5
-fun ConfigurableSource.sourcePreferences(): SharedPreferences =
-    Injekt.get<Application>().getSharedPreferences(preferenceKey(), Context.MODE_PRIVATE)
+fun ConfigurableSource.sourcePreferences(): SharedPreferences = getSourcePreferences()
 
 fun sourcePreferences(key: String): SharedPreferences =
     Injekt.get<Application>().getSharedPreferences(key, Context.MODE_PRIVATE)
