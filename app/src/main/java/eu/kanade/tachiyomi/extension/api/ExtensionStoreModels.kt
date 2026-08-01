475903b52d851cc18d1959258ffea2786297cd6a
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-01 20:54:08 +0530

    Update ExtensionStoreModels.kt

--- app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionStoreModels.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionStoreModels.kt
@@ -18,38 +18,38 @@ data class NetworkExtensionStore(
 ) {
     @Serializable
     data class Contact(
-        @ProtoNumber(1) val website: String,
+        @ProtoNumber(1) val website: String = "",
         @ProtoNumber(2) val discord: String? = null,
     )
 
     @Serializable
     data class ExtensionList(
-        @ProtoNumber(1) val extensions: List<Extension>
+        @ProtoNumber(1) val extensions: List<Extension> = emptyList()
     )
 
     @Serializable
     data class Extension(
-        @ProtoNumber(1) val name: String,
-        @ProtoNumber(2) val packageName: String,
-        @ProtoNumber(3) val resources: Resources,
-        @ProtoNumber(4) val extensionLib: String,
-        @ProtoNumber(5) val versionCode: Long,
-        @ProtoNumber(6) val versionName: String,
-        @ProtoNumber(7) val contentWarning: ContentWarning,
-        @ProtoNumber(8) val sources: List<Source>,
+        @ProtoNumber(1) val name: String = "",
+        @ProtoNumber(2) val packageName: String = "",
+        @ProtoNumber(3) val resources: Resources = Resources(),
+        @ProtoNumber(4) val extensionLib: String = "1.4",
+        @ProtoNumber(5) val versionCode: Long = 0,
+        @ProtoNumber(6) val versionName: String = "",
+        @ProtoNumber(7) val contentWarning: ContentWarning = ContentWarning.UNSPECIFIED,
+        @ProtoNumber(8) val sources: List<Source> = emptyList(),
     )
 
     @Serializable
     data class Resources(
-        @ProtoNumber(1) val apkUrl: String,
-        @ProtoNumber(2) val iconUrl: String,
+        @ProtoNumber(1) val apkUrl: String = "",
+        @ProtoNumber(2) val iconUrl: String = "",
     )
 
     @Serializable
     data class Source(
-        @ProtoNumber(1) val id: Long,
-        @ProtoNumber(2) val name: String,
-        @ProtoNumber(3) val language: String,
+        @ProtoNumber(1) val id: Long = 0,
+        @ProtoNumber(2) val name: String = "",
+        @ProtoNumber(3) val language: String = "",
         @ProtoNumber(4) val homeUrl: String = "",
         @ProtoNumber(5) val mirrorUrls: List<String> = emptyList(),
         @ProtoNumber(7) val message: String? = null,
