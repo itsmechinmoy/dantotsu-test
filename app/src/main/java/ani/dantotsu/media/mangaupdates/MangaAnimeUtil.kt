5cd6bcfc371d85884da23c4bd3dfb79fd2997dc5
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-04 17:48:05 +0530

    Update MangaAnimeUtil.kt

--- app/src/main/java/ani/dantotsu/media/mangaupdates/MangaAnimeUtil.kt
+++ app/src/main/java/ani/dantotsu/media/mangaupdates/MangaAnimeUtil.kt
@@ -176,7 +176,7 @@ object MangaAnimeUtil {
         }
     }
     private val muHeaders = mapOf(
-        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Mobile Safari/537.36",
         "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language" to "en-US,en;q=0.9",
         "Referer" to "https://www.mangaupdates.com/"
