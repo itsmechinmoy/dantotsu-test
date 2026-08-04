124341cda1b24079c7765f4b4eaab12bdd127e4f
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-04 17:46:54 +0530

    Update Preferences.kt

--- app/src/main/java/ani/dantotsu/settings/saving/Preferences.kt
+++ app/src/main/java/ani/dantotsu/settings/saving/Preferences.kt
@@ -32,7 +32,7 @@ enum class PrefName(val data: Pref) {
         Pref(
             Location.General,
             String::class,
-            "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Mobile Safari/537.36"
+            "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Mobile Safari/537.36"
         )
     ),
     AnimeExtensionRepos(Pref(Location.General, Set::class, setOf<String>())),
