72ae0b3fcd2f712fc3059272b728ceeb4ac1375d
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-08 18:50:50 +0530

    Update SearchableSetting.kt

--- app/src/main/java/ani/dantotsu/settings/search/SearchableSetting.kt
+++ app/src/main/java/ani/dantotsu/settings/search/SearchableSetting.kt
@@ -1,15 +1,14 @@
 package ani.dantotsu.settings.search
 
-import androidx.appcompat.app.AppCompatActivity
-import ani.dantotsu.R
+import android.app.Activity
 
 data class SearchableSetting(
     val title: String,
     val desc: String? = null,
-    val icon: Int = R.drawable.ic_round_settings_24,
-    val category: String,
-    val breadcrumbs: String,
-    val targetActivity: Class<out AppCompatActivity>,
-    val highlightKey: String = title,
+    val icon: Int = ani.dantotsu.R.drawable.ic_round_settings_24,
+    val category: String = "",
+    val breadcrumbs: String = "",
+    val targetActivity: Class<out Activity>,
+    val highlightKey: String? = null,
     val isVisible: Boolean = true
 )
