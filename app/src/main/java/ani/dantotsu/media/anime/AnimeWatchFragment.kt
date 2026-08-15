9a1e0c386ba1ea1c535c1128b21e4954992b1712
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-15 09:31:34 +0530

    Update AnimeWatchFragment.kt

--- app/src/main/java/ani/dantotsu/media/anime/AnimeWatchFragment.kt
+++ app/src/main/java/ani/dantotsu/media/anime/AnimeWatchFragment.kt
@@ -72,6 +72,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import tachiyomi.core.util.lang.launchIO
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
@@ -153,6 +154,32 @@ class AnimeWatchFragment : Fragment(), AnimeWatchAdapter.ScanlatorSelectionListe
         }
 
         binding.mediaSourceRecycler.layoutManager = gridLayoutManager
+
+        binding.mediaSourceSwipeRefresh.apply {
+            val primaryColor = PrefManager.getVal<Int>(PrefName.PrimaryColor)
+            setColorSchemeColors(primaryColor)
+            setProgressBackgroundColorSchemeResource(R.color.nav_bg)
+            setOnRefreshListener {
+                if (!this@AnimeWatchFragment::media.isInitialized) {
+                    isRefreshing = false
+                    return@setOnRefreshListener
+                }
+                lifecycleScope.launch(Dispatchers.IO) {
+                    val offline = !isOnline(binding.root.context) || PrefManager.getVal(PrefName.OfflineMode)
+                    val isLocal = model.watchSources?.list?.getOrNull(media.selected!!.sourceIndex)?.name == "Local"
+                    if (!offline && !isLocal) {
+                        val kitsuEpisodes = async { model.loadKitsuEpisodes(media) }
+                        val anifyEpisodes = async { model.loadAnifyEpisodes(media) }
+                        val fillerEpisodes = async { model.loadFillerEpisodes(media) }
+                        awaitAll(kitsuEpisodes, anifyEpisodes, fillerEpisodes)
+                    }
+                    model.loadEpisodes(media, media.selected!!.sourceIndex, invalidate = true)
+                    withContext(Dispatchers.Main) {
+                        binding.mediaSourceSwipeRefresh.isRefreshing = false
+                    }
+                }
+            }
+        }
 
         binding.ScrollTop.setOnClickListener {
             binding.mediaSourceRecycler.scrollToPosition(10)
