c15c7f7d459fedc412778966f45f2be00c833938
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-22 10:08:01 +0530

    Create AppBindings.kt

---
+++ app/src/main/java/ani/dantotsu/di/AppBindings.kt
@@ -0,0 +1,93 @@
+package ani.dantotsu.di
+
+import android.app.Application
+import android.content.Context
+import androidx.media3.database.StandaloneDatabaseProvider
+import ani.dantotsu.addons.download.DownloadAddonManager
+import ani.dantotsu.download.DownloadsManager
+import ani.dantotsu.media.manga.MangaCache
+import ani.dantotsu.parsers.novel.NovelExtensionManager
+import ani.dantotsu.torrent.TorrentServerManager
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.BindingContainer
+import dev.zacsweers.metro.Provides
+import dev.zacsweers.metro.SingleIn
+import eu.kanade.tachiyomi.extension.anime.AnimeExtensionManager
+import eu.kanade.tachiyomi.extension.manga.MangaExtensionManager
+import eu.kanade.tachiyomi.network.JavaScriptEngine
+import eu.kanade.tachiyomi.network.NetworkHelper
+import eu.kanade.tachiyomi.source.anime.AndroidAnimeSourceManager
+import eu.kanade.tachiyomi.source.manga.AndroidMangaSourceManager
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.protobuf.ProtoBuf
+import tachiyomi.domain.source.anime.service.AnimeSourceManager
+import tachiyomi.domain.source.manga.service.MangaSourceManager
+
+@BindingContainer
+object AppBindings {
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesApplication(context: Context): Application = context.applicationContext as Application
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesDownloadsManager(application: Application): DownloadsManager = DownloadsManager(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesNetworkHelper(application: Application): NetworkHelper = NetworkHelper(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesJavaScriptEngine(application: Application): JavaScriptEngine = JavaScriptEngine(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesAnimeExtensionManager(application: Application): AnimeExtensionManager = AnimeExtensionManager(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesMangaExtensionManager(application: Application): MangaExtensionManager = MangaExtensionManager(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesNovelExtensionManager(application: Application): NovelExtensionManager = NovelExtensionManager(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesTorrentServerManager(application: Application): TorrentServerManager = TorrentServerManager(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesDownloadAddonManager(application: Application): DownloadAddonManager = DownloadAddonManager(application)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesAnimeSourceManager(application: Application, extensionManager: AnimeExtensionManager): AnimeSourceManager =
+        AndroidAnimeSourceManager(application, extensionManager)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesMangaSourceManager(application: Application, extensionManager: MangaExtensionManager): MangaSourceManager =
+        AndroidMangaSourceManager(application, extensionManager)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesMangaCache(): MangaCache = MangaCache()
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesDatabaseProvider(context: Context): StandaloneDatabaseProvider = StandaloneDatabaseProvider(context)
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesJson(): Json = Json {
+        ignoreUnknownKeys = true
+        explicitNulls = false
+    }
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun providesProtoBuf(): ProtoBuf = ProtoBuf
+}
