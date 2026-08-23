cb4bd12809677d39e5adb237377ddd48790df248
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-23 20:52:55 +0530

    Update Preferences.kt

--- app/src/main/java/ani/dantotsu/settings/saving/Preferences.kt
+++ app/src/main/java/ani/dantotsu/settings/saving/Preferences.kt
@@ -73,6 +73,7 @@ enum class PrefName(val data: Pref) {
     UseMaterialYou(Pref(Location.UI, Boolean::class, false)),
     Theme(Pref(Location.UI, String::class, "PURPLE")),
     SkipExtensionIcons(Pref(Location.UI, Boolean::class, false)),
+    SecureExtensionScreenshots(Pref(Location.UI, Boolean::class, true)),
     DarkMode(Pref(Location.UI, Int::class, 0)),
     ShowYtButton(Pref(Location.UI, Boolean::class, true)),
     AnimeDefaultView(Pref(Location.UI, Int::class, 0)),
