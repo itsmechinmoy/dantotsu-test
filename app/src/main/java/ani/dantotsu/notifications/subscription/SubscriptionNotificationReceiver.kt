09f7926b733071eb0cc8116345ea53d468b00ad7
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-23 22:17:29 +0530

    Update SubscriptionNotificationReceiver.kt

--- app/src/main/java/ani/dantotsu/notifications/subscription/SubscriptionNotificationReceiver.kt
+++ app/src/main/java/ani/dantotsu/notifications/subscription/SubscriptionNotificationReceiver.kt
@@ -8,19 +8,28 @@ import ani.dantotsu.notifications.TaskScheduler
 import ani.dantotsu.settings.saving.PrefManager
 import ani.dantotsu.settings.saving.PrefName
 import ani.dantotsu.util.Logger
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SubscriptionNotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         Logger.log("SubscriptionNotificationReceiver: onReceive")
-        runBlocking {
-            SubscriptionNotificationTask().execute(context)
+        val pendingResult = goAsync()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                SubscriptionNotificationTask().execute(context)
+                val subscriptionInterval =
+                    SubscriptionNotificationWorker.checkIntervals[PrefManager.getVal(PrefName.SubscriptionNotificationInterval)]
+                AlarmManagerScheduler(context).scheduleRepeatingTask(
+                    TaskScheduler.TaskType.SUBSCRIPTION_NOTIFICATION,
+                    subscriptionInterval
+                )
+            } catch (e: Exception) {
+                Logger.log(e)
+            } finally {
+                pendingResult.finish()
+            }
         }
-        val subscriptionInterval =
-            SubscriptionNotificationWorker.checkIntervals[PrefManager.getVal(PrefName.SubscriptionNotificationInterval)]
-        AlarmManagerScheduler(context).scheduleRepeatingTask(
-            TaskScheduler.TaskType.SUBSCRIPTION_NOTIFICATION,
-            subscriptionInterval
-        )
     }
 }
