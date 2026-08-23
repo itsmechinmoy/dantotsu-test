b0b597b7e038265ef940206b38755c0403ae4ccc
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-23 22:18:12 +0530

    Update CommentNotificationReceiver.kt

--- app/src/main/java/ani/dantotsu/notifications/comment/CommentNotificationReceiver.kt
+++ app/src/main/java/ani/dantotsu/notifications/comment/CommentNotificationReceiver.kt
@@ -8,19 +8,28 @@ import ani.dantotsu.notifications.TaskScheduler
 import ani.dantotsu.settings.saving.PrefManager
 import ani.dantotsu.settings.saving.PrefName
 import ani.dantotsu.util.Logger
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CommentNotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         Logger.log("CommentNotificationReceiver: onReceive")
-        runBlocking {
-            CommentNotificationTask().execute(context)
+        val pendingResult = goAsync()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                CommentNotificationTask().execute(context)
+                val commentInterval =
+                    CommentNotificationWorker.checkIntervals[PrefManager.getVal(PrefName.CommentNotificationInterval)]
+                AlarmManagerScheduler(context).scheduleRepeatingTask(
+                    TaskScheduler.TaskType.COMMENT_NOTIFICATION,
+                    commentInterval
+                )
+            } catch (e: Exception) {
+                Logger.log(e)
+            } finally {
+                pendingResult.finish()
+            }
         }
-        val commentInterval =
-            CommentNotificationWorker.checkIntervals[PrefManager.getVal(PrefName.CommentNotificationInterval)]
-        AlarmManagerScheduler(context).scheduleRepeatingTask(
-            TaskScheduler.TaskType.COMMENT_NOTIFICATION,
-            commentInterval
-        )
     }
 }
