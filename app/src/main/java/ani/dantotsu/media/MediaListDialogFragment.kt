68663afd6a1218e9af7ee7fbdf959db8d12c8cea
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-24 18:01:07 +0530

    Update MediaListDialogFragment.kt

--- app/src/main/java/ani/dantotsu/media/MediaListDialogFragment.kt
+++ app/src/main/java/ani/dantotsu/media/MediaListDialogFragment.kt
@@ -222,9 +222,9 @@ class MediaListDialogFragment : BottomSheetDialogFragment() {
                 binding.mediaListPrivate.setOnCheckedChangeListener { _, checked ->
                     media?.isListPrivate = checked
                 }
-                val removeList = PrefManager.getCustomVal("removeList", setOf<Int>())
+                val removeList = PrefManager.getCustomVal<Set<String>>("removeList", emptySet())
                 var remove: Boolean? = null
-                binding.mediaListShow.isChecked = media?.id in removeList
+                binding.mediaListShow.isChecked = media?.id?.toString() in removeList
                 binding.mediaListShow.setOnCheckedChangeListener { _, checked ->
                     remove = checked
                 }
@@ -326,9 +326,9 @@ class MediaListDialogFragment : BottomSheetDialogFragment() {
                             }
                         }
                         if (remove == true) {
-                            PrefManager.setCustomVal("removeList", removeList.plus(media!!.id))
+                            PrefManager.setCustomVal("removeList", removeList.plus(media!!.id.toString()))
                         } else if (remove == false) {
-                            PrefManager.setCustomVal("removeList", removeList.minus(media!!.id))
+                            PrefManager.setCustomVal("removeList", removeList.minus(media!!.id.toString()))
                         }
                         Refresh.all()
                         snackString(getString(R.string.list_updated))
