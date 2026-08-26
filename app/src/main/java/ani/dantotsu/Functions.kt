c4a625e838e707baf2b4e4c1834d0263c710d3fb
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-26 22:23:08 +0530

    Update Functions.kt

--- app/src/main/java/ani/dantotsu/Functions.kt
+++ app/src/main/java/ani/dantotsu/Functions.kt
@@ -52,6 +52,7 @@ import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.ViewTreeObserver
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
