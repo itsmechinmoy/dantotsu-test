3b986bb812b29d102ecc2e6bd569bf65e02787c4
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-26 22:03:37 +0530

    Update ActivityReplyItem.kt

--- app/src/main/java/ani/dantotsu/profile/activity/ActivityReplyItem.kt
+++ app/src/main/java/ani/dantotsu/profile/activity/ActivityReplyItem.kt
@@ -36,7 +36,7 @@ class ActivityReplyItem(
     override fun bind(viewBinding: ItemActivityReplyBinding, position: Int) {
         binding = viewBinding
         val context = binding.root.context
-        val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+        val scope = fragActivity.lifecycleScope
         binding.activityUserAvatar.loadImage(reply.user.avatar?.medium)
         binding.activityUserName.text = reply.user.name
         binding.activityTime.text = ActivityItemBuilder.getDateTime(reply.createdAt)
