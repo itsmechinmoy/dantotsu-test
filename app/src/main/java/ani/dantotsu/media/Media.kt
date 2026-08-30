765fe4f7f8a3d51771aee371353fdcb6600a50dd
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-30 21:22:28 +0530

    Add clearLogo property to Media class

--- app/src/main/java/ani/dantotsu/media/Media.kt
+++ app/src/main/java/ani/dantotsu/media/Media.kt
@@ -43,6 +43,7 @@ data class Media(
 
     var cover: String? = null,
     var banner: String? = null,
+    var clearLogo: String? = null,
     var relation: String? = null,
     var favourites: Int? = null,
 
