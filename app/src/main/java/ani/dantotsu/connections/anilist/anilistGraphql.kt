99733f6952418c54920e765d3cfa38b171a2f50a
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-31 10:15:25 +0530

    Update GraphQL queries to use version 2 and 3

--- app/src/main/java/ani/dantotsu/connections/anilist/anilistGraphql.kt
+++ app/src/main/java/ani/dantotsu/connections/anilist/anilistGraphql.kt
@@ -56,7 +56,7 @@ fun characterInformation(includeMediaInfo: Boolean) = """
           id
           idMal
           isAdult
-          status
+          status(version: 2)
           chapters
           episodes
           nextAiringEpisode { episode }
@@ -98,7 +98,7 @@ fun studioInformation(page: Int, perPage: Int) = """
           id
           idMal
           isAdult
-          status
+          status(version: 2)
           chapters
           episodes
           nextAiringEpisode { episode }
@@ -163,7 +163,7 @@ fun staffInformation(page: Int, perPage: Int) = """
           id
           idMal
           isAdult
-          status
+          status(version: 2)
           chapters
           episodes
           nextAiringEpisode { episode }
@@ -393,7 +393,7 @@ fun fullMediaInformation(id: Int) = """
     }
     relations {
       edges {
-        relationType(version: 2)
+        relationType(version: 3)
         node {
           ${standardMediaInformation()}
         }
