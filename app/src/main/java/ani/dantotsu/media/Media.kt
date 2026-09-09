d6b937c5354145dcfa0d5b7505521c61d00fb654
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-09-09 12:27:24 +0530

    Add TMDB ID to Media data class

--- app/src/main/java/ani/dantotsu/media/Media.kt
+++ app/src/main/java/ani/dantotsu/media/Media.kt
@@ -103,6 +103,7 @@ data class Media(
     var idKitsu: String? = null,
     var externalLinks: ArrayList<MediaExternalLink>? = null,
     var idIMDB: String? = null,
+    var idTMDB: String? = null,
 
     var cameFromContinue: Boolean = false
 ) : Serializable {
