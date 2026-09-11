2ac74824857d348f429c9c215cb4c5e4d6e6c1ed
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-09-11 14:53:02 +0530

    Enhance headers in Anilist API requests
    Added Referer and Origin headers for requests.

--- app/src/main/java/ani/dantotsu/connections/anilist/Anilist.kt
+++ app/src/main/java/ani/dantotsu/connections/anilist/Anilist.kt
@@ -343,7 +343,9 @@ object Anilist {
             }
             val headers = mutableMapOf(
                 "Content-Type" to "application/json; charset=utf-8",
-                "Accept" to "application/json"
+                "Accept" to "application/json",
+                "Referer" to "https://anilist.co/",
+                "Origin" to "https://anilist.co"
             )
 
             if (token != null || force) {
