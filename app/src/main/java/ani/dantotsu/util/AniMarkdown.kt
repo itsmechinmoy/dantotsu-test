94d877a6f6eb6fca16422964e2d1516ea4c14f0b
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-09-17 15:21:21 +0530

    Enhance YouTube embed functionality and styling
    Updated convertYoutubeToHtml function to support interactive mode and added responsive design for YouTube embeds.

--- app/src/main/java/ani/dantotsu/util/AniMarkdown.kt
+++ app/src/main/java/ani/dantotsu/util/AniMarkdown.kt
@@ -38,19 +38,23 @@ class AniMarkdown { //istg anilist has the worst api
             }
         }
 
-        private fun String.convertYoutubeToHtml(): String {
+        private fun String.convertYoutubeToHtml(interactive: Boolean = false): String {
             val divRegex = """<div class='youtube' id='(.*?)'></div>""".toRegex()
             val markdownRegex = """youtube\((.*?)\)""".toRegex()
 
             fun getYoutubeEmbedHtml(url: String): String {
                 val id = getYoutubeId(url)
                 return if (id.isNotEmpty()) {
-                    """<div>
-                    <a href="https://www.youtube.com/watch?v=$id"><img src="https://i3.ytimg.com/vi/$id/maxresdefault.jpg" alt="$url"></a>
-                    <align center>
-                    <a href="https://www.youtube.com/watch?v=$id">Youtube Link</a>
-                    </align>
-                    </div>""".trimIndent()
+                    if (interactive) {
+                        """<div class="yt-embed" data-id="$id" onclick="this.innerHTML='<iframe src=\'https://www.youtube-nocookie.com/embed/$id?autoplay=1&amp;rel=0&amp;modestbranding=1&amp;controls=1\' allow=\'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\' allowfullscreen frameborder=\'0\'></iframe>';"><img src="https://img.youtube.com/vi/$id/hqdefault.jpg" alt="$url"><div class="yt-play-btn"><div class="yt-play-icon"></div></div></div>""".trimIndent()
+                    } else {
+                        """<div>
+                        <a href="https://www.youtube.com/watch?v=$id"><img src="https://i3.ytimg.com/vi/$id/maxresdefault.jpg" alt="$url"></a>
+                        <align center>
+                        <a href="https://www.youtube.com/watch?v=$id">Youtube Link</a>
+                        </align>
+                        </div>""".trimIndent()
+                    }
                 } else {
                     """<a href="$url">Youtube Video</a>"""
                 }
@@ -113,8 +117,15 @@ class AniMarkdown { //istg anilist has the worst api
         }
 
         fun getFullAniHTML(html: String, textColor: Int): String {
-            val basicHtml = getBasicAniHTML(html)
-
+            val contentHtml = html
+                .convertNestedImageToHtml()
+                .convertImageToHtml()
+                .convertLinkToHtml()
+                .convertYoutubeToHtml(interactive = true)
+                .convertWebmToHtml()
+                .convertCenterToHtml()
+                .replaceLeftovers()
+                .underlineToHtml()
 
             val returnHtml = """
             <html>
@@ -140,10 +151,56 @@ class AniMarkdown { //istg anilist has the worst api
             a {
                 color: ${textColor.toCssColor()};
             }
-            /* Add responsive design elements for other content as needed */
+            .yt-embed {
+                position: relative;
+                width: 100%;
+                aspect-ratio: 16 / 9;
+                background: #000;
+                border-radius: 12px;
+                overflow: hidden;
+                cursor: pointer;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                margin: 12px 0;
+                -webkit-tap-highlight-color: transparent;
+            }
+            .yt-embed img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+            .yt-play-btn {
+                position: absolute;
+                width: 68px;
+                height: 48px;
+                background: rgba(255, 0, 0, 0.85);
+                border-radius: 12px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                box-shadow: 0 4px 12px rgba(0,0,0,0.5);
+                pointer-events: none;
+            }
+            .yt-play-icon {
+                width: 0;
+                height: 0;
+                border-left: 20px solid white;
+                border-top: 12px solid transparent;
+                border-bottom: 12px solid transparent;
+                margin-left: 4px;
+            }
+            .yt-embed iframe {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                border: none;
+            }
         </style>
 </head>
-<body>$basicHtml</body>
+<body>$contentHtml</body>
 </html>
     """.trimIndent()
             return returnHtml
