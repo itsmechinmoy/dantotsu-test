bece6a968d6c6ab39748077d7275c01955d9169c
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-09-22 14:36:49 +0530

    Add data classes for NetworkAnimeExtensionStore

--- app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionStoreModels.kt
+++ app/src/main/java/eu/kanade/tachiyomi/extension/api/ExtensionStoreModels.kt
@@ -76,6 +76,92 @@ data class NetworkExtensionStore(
     }
 }
 
+@SuppressLint("UnsafeOptInUsageError")
+@Serializable
+data class NetworkAnimeExtensionStore(
+    @ProtoNumber(1) val name: String = "",
+    @ProtoNumber(2) val badgeLabel: String = "",
+    @ProtoNumber(3) val signingKey: String = "",
+    @ProtoNumber(4) val contact: Contact = Contact(),
+    @ProtoNumber(101) val extensionList: ExtensionList? = null,
+    @ProtoNumber(102) val extensionListUrl: String? = null,
+) {
+    @Serializable
+    data class Contact(
+        @ProtoNumber(1) val website: String = "",
+        @ProtoNumber(2) val discord: String? = null,
+    )
+
+    @Serializable
+    data class ExtensionList(
+        @ProtoNumber(1) val extensions: List<Extension> = emptyList()
+    )
+
+    @Serializable
+    data class Extension(
+        @ProtoNumber(1) val name: String = "",
+        @ProtoNumber(2) val packageName: String = "",
+        @ProtoNumber(3) val resources: Resources = Resources(),
+        @ProtoNumber(4) val extensionLib: String = "",
+        @ProtoNumber(5) val versionCode: Long = 0,
+        @ProtoNumber(6) val versionName: String = "",
+        @ProtoNumber(7) val contentWarning: ContentWarning = ContentWarning.UNSPECIFIED,
+        @ProtoNumber(8) val isTorrent: Boolean = false,
+        @ProtoNumber(9) val sources: List<Source> = emptyList(),
+    )
+
+    @Serializable
+    data class Resources(
+        @ProtoNumber(1) val apkUrl: String = "",
+        @ProtoNumber(2) val iconUrl: String = "",
+    )
+
+    @Serializable
+    data class Source(
+        @ProtoNumber(1) val id: Long = 0,
+        @ProtoNumber(2) val name: String = "",
+        @ProtoNumber(3) val language: String = "",
+        @ProtoNumber(4) val homeUrl: String = "",
+        @ProtoNumber(5) val mirrorUrls: List<String> = emptyList(),
+        @ProtoNumber(6) val message: String? = null,
+    )
+
+    @Suppress("Unused")
+    @Serializable
+    enum class ContentWarning {
+        @ProtoNumber(0)
+        @JsonNames("CONTENT_WARNING_UNSPECIFIED")
+        UNSPECIFIED,
+
+        @ProtoNumber(1)
+        @JsonNames("CONTENT_WARNING_SAFE")
+        SAFE,
+
+        @ProtoNumber(2)
+        @JsonNames("CONTENT_WARNING_MIXED")
+        MIXED,
+
+        @ProtoNumber(3)
+        @JsonNames("CONTENT_WARNING_NSFW")
+        NSFW,
+    }
+}
+
+@SuppressLint("UnsafeOptInUsageError")
+@Serializable
+data class NetworkExtensionStoreMetaOnly(
+    @ProtoNumber(1) val name: String = "",
+    @ProtoNumber(2) val badgeLabel: String = "",
+    @ProtoNumber(3) val signingKey: String = "",
+    @ProtoNumber(4) val contact: Contact = Contact(),
+) {
+    @Serializable
+    data class Contact(
+        @ProtoNumber(1) val website: String = "",
+        @ProtoNumber(2) val discord: String? = null,
+    )
+}
+
 @Serializable
 data class NetworkLegacyExtensionRepo(
     @SerialName("index_v2")
